47d8def87854fe7dc6b0f21e2ecbe1384602d20e
WJ <WJ@Kees-Air.lan>
2020-05-28 09:57:07 +0800

    .

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var SpaceHipster = SpaceHipster || {};
 
 //initiate Phaser framework
-SpaceHipster = new Phaser.Game('200','200',Phaser.AUTO);
+SpaceHipster.game = new Phaser.Game('100%','100%',Phaser.AUTO);
 
-SpaceHipster.state.add('GameState',SpaceHipster);
-SpaceHipster.state.start('GameState')
+SpaceHipster.game.state.add('GameState',SpaceHipster.GameState);
+SpaceHipster.game.state.start('GameState')
